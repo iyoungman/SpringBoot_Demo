e26ecafcf2c212797dd6ac8339c61ca70c6498ba
iyoungman <hsl9546@gmail.com>
2019-02-01 02:22:42 +0900

    cosmos-demo 추가

---
+++ cosmos-demo/src/main/java/com/youngman/cosmosdemo/model/Receiver.java
@@ -0,0 +1,49 @@
+package com.youngman.cosmosdemo.model;
+
+import com.youngman.cosmosdemo.model.embedded.Address;
+import com.youngman.cosmosdemo.model.embedded.Name;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+/**
+ * Created by youngman on 2019-01-29.
+ */
+@Getter
+@Setter
+@Entity
+@ToString
+public class Receiver {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Embedded
+    private Name name;
+
+    /*
+     * 중복적인 칼럼들을 자료형으로 규합 -> 객체지향
+     * 참조 클래스의 필드를 그대로 자신으로 컬럼으로 만들고 싶을 때
+     * @Embedded 값 타입을 사용한다고 명시
+     * @AttributeOverrides : Embedded 타입 Mapping 정보 재정의
+     */
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "city", column = @Column(name = "receiver_city")),
+            @AttributeOverride(name = "groop", column = @Column(name = "receiver_groop")),
+            @AttributeOverride(name = "dong", column = @Column(name = "receiver_dong"))
+    })
+    private Address address;
+
+    public Receiver() {
+    }
+
+    @Builder
+    public Receiver(Name name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+}
---
+++ cosmos-demo/src/test/java/com/youngman/cosmosdemo/CrudTest.java
@@ -0,0 +1,139 @@
+package com.youngman.cosmosdemo;
+
+import com.youngman.cosmosdemo.model.Profile;
+import com.youngman.cosmosdemo.model.Receiver;
+import com.youngman.cosmosdemo.model.Sender;
+import com.youngman.cosmosdemo.model.embedded.Address;
+import com.youngman.cosmosdemo.model.embedded.Name;
+import com.youngman.cosmosdemo.repository.ProfileRepository;
+import com.youngman.cosmosdemo.repository.ReceiverRepository;
+import com.youngman.cosmosdemo.repository.SenderRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by youngman on 2019-01-29.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrudTest {
+    private Logger logger = LoggerFactory.getLogger(CrudTest.class);
+
+    @Autowired
+    private ReceiverRepository receiverRepository;
+
+    @Autowired
+    private SenderRepository senderRepository;
+
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    @Test
+    public void saveReceiver() {
+        Name name = Name.builder()
+                .lastName("이")
+                .firstName("영준")
+                .build();
+        Address address = Address.builder()
+                .city("서울")
+                .groop("노원구")
+                .dong("중계동")
+                .build();
+
+        receiverRepository.save(Receiver.builder()
+                .name(name)
+                .address(address)
+                .build());
+    }
+
+    @Test
+    public void saveSender() {
+        Name name = Name.builder()
+                .lastName("박")
+                .firstName("춘소")
+                .build();
+        Address address = Address.builder()
+                .city("경기도")
+                .groop("양평읍")
+                .dong("용문동")
+                .build();
+
+        senderRepository.save(Sender.builder()
+                .name(name)
+                .address(address)
+                .build());
+    }
+
+    @Test
+    public void findIdByName() {
+        Name name = Name.builder()
+                .lastName("이")
+                .firstName("영준")
+                .build();
+
+        Long id = receiverRepository.findIdByName(name);
+        logger.info("================이름으로 기본키 찾기=================== : " + id.toString());
+    }
+
+    @Test
+    public void saveProfile() {
+        Receiver receiver = new Receiver();
+        receiver.setId(1L);
+
+        Profile profile = Profile.builder()
+                .current(true)
+                .receiver(receiver)
+                .build();
+
+        profileRepository.save(profile);
+    }
+
+    @Test
+    public void getProfile() {
+        Profile profile = profileRepository.findProfileById(1L);
+        logger.info("================프로필 출력===================");
+        logger.info(profile.printToProfile());
+    }
+
+    /*
+     * p156 - 단방향인데 회원 정보를 조회하면서 그 회원의 프로필 정보 어떻게 볼까?
+     */
+    @Test
+    public void getReceiverWithProfileCount() {
+        Name name = Name.builder()
+                .lastName("박")
+                .firstName("영준")
+                .build();
+
+        List<Object[]> lists = receiverRepository.getReceiverWithProfileCount(name);
+        logger.info("==============이름으로 멤버와 프로필 출력하기================");
+        lists.forEach(arr -> logger.info(Arrays.toString(arr)));
+    }
+
+    @Test
+    public void getReceiverWithProfile() {
+        Name name = Name.builder()
+                .lastName("박")
+                .firstName("영준")
+                .build();
+
+        List<Object[]> lists = receiverRepository.getReceiverWithProfile(name);
+        logger.info("==============이름으로 멤버와 프로필 출력하기================");
+        lists.forEach(arr -> logger.info(Arrays.toString(arr)));
+    }
+
+    @Test
+    public void getAllReceiverWithProfile() {
+        List<Object[]> lists = receiverRepository.getAllReceiverWithProfile();
+        logger.info("==============전체 멤버와 프로필 출력하기================");
+        lists.forEach(arr -> logger.info(Arrays.toString(arr)));
+    }
+}
